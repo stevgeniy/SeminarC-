59c4c62cddd127eb29a0609698347923b4d18440
stevgeniy <stepin_evgen@mail.ru>
2023-03-09 22:42:01 +0300

    Exampl_001

---
+++ Exampl_001/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число A: ");
+
+int numa = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите целое число B: ");
+
+int numb = int.Parse(Console.ReadLine() ??"");
+
+/////////////////////////
+
+if (numa==numb*numb)
+{
+    Console.WriteLine($"{numa} -> {numb}  ДА");
+}
+
+else
+{
+    Console.WriteLine($"{numa} -> {numb}  НЕТ");
+}
