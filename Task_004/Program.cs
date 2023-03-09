ef78e407d1c689a720cf5b91291fd7b0c9c6d9d5
stevgeniy <stepin_evgen@mail.ru>
2023-03-10 00:09:52 +0300

    Task_04

---
+++ Task_004/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число: ");
+
+int Num1 = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите второе число: ");
+
+int Num2 = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите третье число: ");
+
+int Num3 = int.Parse(Console.ReadLine() ??"");
+
+int Max = Num1;
+
+if (Num2 > Max)
+{
+    Max = Num2;
+}
+
+if (Num3 > Max)
+{
+    Max = Num3;
+}
+
+Console.WriteLine($"{Max} - > Наибольшее число");
