d2d2d04fe4b9e53497946aa880935a9b833e81b1
stevgeniy <stepin_evgen@mail.ru>
2023-03-09 22:23:51 +0300

    Qadro3

--- Qadro/Program.cs
+++ Qadro/Program.cs
@@ -2,6 +2,11 @@
 
 int userNumber = int.Parse(Console.ReadLine() ??"");
 
-int result = userNumber*userNumber;
+/////////////////////////
+
+//int result = userNumber*userNumber;
+int result = (int)Math.Pow(userNumber, 2);
+
+/////////////////////////
 
 Console.WriteLine($"{userNumber} -> {result}");
