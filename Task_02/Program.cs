cf345aaf308b8b2ab7be6ea1addc8738d0667fae
stevgeniy <stepin_evgen@mail.ru>
2023-03-09 23:59:25 +0300

    Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое первое число: ");
+
+int Num1 = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите второе число: ");
+
+int Num2 = int.Parse(Console.ReadLine() ??"");
+
+/////////////////////////
+
+if (Num2 > Num1)
+{
+    Console.WriteLine($"{Num2} -> Наибольшее число");
+}
+
+else
+{
+    Console.WriteLine($"{Num1} -> Наибольшее число");
+}
