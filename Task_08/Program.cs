5322aa3d732905c6889a4c9bf92346f790668d91
stevgeniy <stepin_evgen@mail.ru>
2023-03-10 00:28:49 +0300

    Task_08

---
+++ Task_08/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое число: ");
+
+int Number = int.Parse(Console.ReadLine() ?? "");
+
+/// Начало цикла
+
+int count = 1;
+Console.WriteLine("Четные числа: ");
+
+while (count <= Number)
+{
+        if (count % 2 == 0)
+    {
+            Console.Write($"{count},");
+    }
+    count = count + 1;
+}
