070345f1eda307da6c1ba5e8c1cf6849698e5d54
stevgeniy <stepin_evgen@mail.ru>
2023-03-09 23:31:40 +0300

    Exampl_002

---
+++ Exampl_002/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+Console.Write("Введите номер дня недели от 1 до 7: ");
+
+int userDay = int.Parse(Console.ReadLine() ?? "");
+
+if (userDay == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+else if (userDay == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+else if (userDay == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+else if (userDay == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+else if (userDay == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+else if (userDay == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+else if (userDay == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+else
+{
+    Console.WriteLine("Неверноое число");
+}
