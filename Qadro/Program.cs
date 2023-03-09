84acacbcefcfbe5042b6b4c867caac801fa55beb
stevgeniy <stepin_evgen@mail.ru>
2023-03-09 22:14:48 +0300

    Qadro2

--- Qadro/Program.cs
+++ Qadro/Program.cs
@@ -1,8 +1,6 @@
 ﻿Console.Write("Введите целое число: ");
 
-string userInputStr = Console.ReadLine() ??"";
-
-int userNumber = int.Parse(userInputStr);
+int userNumber = int.Parse(Console.ReadLine() ??"");
 
 int result = userNumber*userNumber;
 
