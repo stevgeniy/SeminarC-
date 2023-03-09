0b776c8439e69fe3b367c46e2b3164a54a85e27c
stevgeniy <stepin_evgen@mail.ru>
2023-03-10 00:14:34 +0300

    Task_06

---
+++ Task_06/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое число: ");
+
+int Num1 = int.Parse(Console.ReadLine() ??"");
+
+
+if (Num1%2 == 0)
+{
+    Console.WriteLine($"{Num1} -> Четное");
+}
+
+else
+{
+    Console.WriteLine($"{Num1} -> Нечетное");
+}
